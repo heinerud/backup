19b584f510bf990055c7b6b954135b4ec92a01ea
heinerud <joel.heinerud@gmail.com>
2020-02-21 00:19:33 +0100

    Update notify_login.sh

--- etc/profile.d/notify_login.sh
+++ etc/profile.d/notify_login.sh
@@ -1,1 +1,7 @@
+ip=$(echo $SSH_CONNECTION | cut -d " " -f 1)
+host=$(hostname)
+msg="$host: user $USER just logged in from $ip"
+token=<your-bot-token>
+chat_id=<your-user-number>
 
+curl -s "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$msg" > /dev/null
